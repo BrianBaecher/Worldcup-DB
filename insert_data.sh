9eba80287cd78f98fca67ffc29d43ca452602c3f
Brian Baecher <brian.baecher@gmail.com>
2022-08-01 18:31:22 -0400

    added sql file and satisfied coderoad tests

--- insert_data.sh
+++ insert_data.sh
@@ -13,10 +13,10 @@ cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPON
         #add each team to teams table
         #I'll do the winner first
         INSERT_WINNER=$($PSQL "INSERT INTO teams (name) VALUES ('$WINNER')")
-        if [[ $INSERT_WINNER == 'INSERT 0 1' ]]; then
+        if [[ $INSERT_WINNER =~ ^INSERT ]]; then
             echo Added $WINNER
         #Below is not working
-        #elif [[ $INSERT_WINNER =~ "ERROR:  duplicate key value violates unique constraint "teams_name_key"" ]]
+        #elif [[ $INSERT_WINNER =~ ^ERROR: ]]
         #then echo $WINNER has already been added
         fi
         INSERT_OPPONENT=$($PSQL "INSERT INTO teams (name) VALUES ('$OPPONENT')")
@@ -24,16 +24,16 @@ cat games.csv | while IFS=',' read YEAR ROUND WINNER OPPONENT WINNER_GOALS OPPON
             echo $OPPONENT added
         fi
         : ' WINNER and OPPONENT should be added to the teams table now -
-        Next insert the info for the games table -- columns for that table are below ---
+        Next insert the info for the games table
+        games table columns are ---
         year, round, winner_id, opponent_id '
         #GET WINNER ID
         GET_WINNER_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$WINNER'")
-        #GET OPP ID
         GET_OPPONENT_ID=$($PSQL "SELECT team_id FROM teams WHERE name = '$OPPONENT'")
 
         INSERT_GAMES=$($PSQL "INSERT INTO games (year, round, winner_id, opponent_id, winner_goals, opponent_goals) VALUES ('$YEAR' , '$ROUND' , '$GET_WINNER_ID', '$GET_OPPONENT_ID', '$WINNER_GOALS' , '$OPPONENT_GOALS')")
         if [[ $INSERT_GAMES == 'INSERT 0 1' ]]; then
-            echo Game added
+            echo $YEAR , $ROUND added
         fi
     fi
 
